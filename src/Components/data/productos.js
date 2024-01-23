e9e8004c0ab532c7744c1cf7e6841fc88aa710bb
Cecilia <cecilia.perdomo@gmail.com>
2024-01-22 23:07:48 -0300

    Productos

---
+++ src/Components/data/productos.js
@@ -0,0 +1,32 @@
+export const mockData = [
+    {
+        title: "Televisor Led Smart LG 43'' 4K", 
+        description: "Viví una experiencia totalmente inmersiva y de calidad impresionante con este smart tv LG 4K UHD de 43'. Una imagen perfecta para los que son exigentes al ver sus contenidos de la mejor manera. Cuenta con múltiples conexiones y un sonido sorround. Los televisores LG UHD superan tus expectativas en todo momento. Experimentá una calidad de imagen más real, con colores vivos y una precisión cuatro veces superior a la del Full HD.",
+        inStock: 100,
+        price: 499,
+        slug: "tele1",
+        image: "tvs/tele1.jpeg",
+        type: "tvs",
+        destacado: "si"
+    }, 
+    {
+        title: "Smart TV TCL 55” QLED 4K UHD Sistema Google TV HDR10+", 
+        description: "El Smart TV TCL QLED 4K te ofrece una calidad de imagen sobresaliente y un audio de alta fidelidad con la ayuda de Quantum Dot y ONKYO Sound System respectivamente. Posee una gran experiencia de usuario súper fácil, rápida y conveniente que se beneficia del sistema de Google TV. Despertá tu Google TV C635 personalizado diciendo '¡OK Google!', comenzá tu increíble viaje a la televisión inteligente.",
+        inStock: 100,
+        price: 579,
+        slug: "tele2",
+        image: "tvs/tele2.jpeg",
+        type: "tvs",
+        destacado: "si"
+    }, 
+    {
+        title: "Smart TV SAMSUNG 50' 4K Crystal UHD Nuevo Modelo CU7000 Slim Look", 
+        description: "Disfrutá de tus programas favoritos en la mejor calidad con esta Smart TV Samsung de 50'. El procesador Crystal 4K UHD y la tecnología PurColor te permitirán visualizar un billón de colores en un espectro más amplio que los modelos RGB tradicionales con una potente resolución en 4K. Gracias al increíble sonido y el modo cineasta podrás deleitarte como si estuvieses en un cine pero con la comodidad de tu hogar. Además, podrás encontrar todo lo que deseás ver fácilmente con Smart Hub.",
+        inStock: 100,
+        price: 519,
+        slug: "tele3",
+        image: "tvs/tele3.jpeg",
+        type: "tvs",
+        destacado: "no"
+    }, 
+]
