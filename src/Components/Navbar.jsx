b538f7361b277c1c8ad225108c71ee62aaffde11
Cecilia <cecilia.perdomo@gmail.com>
2024-01-25 17:07:29 -0300

    Comienzo productos

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     const links = [
         {
             label: "Productos",
-            href: "/"
+            href: "/productos"
         },
 
         {
---
+++ src/app/productos/page.js
@@ -0,0 +1,12 @@
+const Productos = () => {
+    return (
+        <>
+            <div className="container m-auto">
+                <h1 className="text-2xl text-bold">Nuestros productos:</h1>
+                <hr />
+
+                </div>
+        </>
+    )
+}
+export default Productos
