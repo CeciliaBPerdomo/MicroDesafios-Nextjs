41338f3a62840cdf7fc0e9bf51f13e6192f43348
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-02-14 19:44:20 -0300

    Configuracion storage

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,6 +15,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
-
-export { db }
+export const db = getFirestore(app)
+export const storage = getStorage(app)
