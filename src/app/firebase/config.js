5252a8528876ce82d7828e64671b6a9a2de566a6
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-02-15 10:50:31 -0300

    Firebase Auth

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
-import { getStorage } from "firebase/storage"
+import { getStorage } from "firebase/storage" // Guarda las imagenes
+import {getAuth} from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,3 +18,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 export const storage = getStorage(app)
+export const auth = getAuth(app)
