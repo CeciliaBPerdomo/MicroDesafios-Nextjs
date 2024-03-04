7a667a6f7cd71c10fbc66cc386ba60c38cc402e5
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-03-04 13:09:20 -0300

    Margenes Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
         <>
             <footer className="w-full bg-red-100 border-t border-red-600 mt-10">
                 <div className='container m-auto py-2 text-sm text-black flex justify-between items-center'>
-                    <p>Desarrollado con ❤️ por <b>Cecilia Perdomo</b></p>
+                    <p className='mr-4'>Desarrollado con ❤️ por <b>Cecilia Perdomo</b></p>
                     
                     <div className='flex items-center gap-2'>
-                        <p className='text-black'>Powered by</p>
+                        <p className='text-black ml-2'>Powered by</p>
                         <Image
                             src={"/Logo.png"}
                             alt={"Logo"}
