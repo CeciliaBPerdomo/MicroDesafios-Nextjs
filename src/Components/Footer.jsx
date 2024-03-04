4fc7c023c24927f9825d6e00ded026e22e2d4863
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-03-04 18:14:19 -0300

    Modificacion footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link';
 
 const Footer = () => {
     return (
         <>
             <footer className="w-full bg-red-100 border-t border-red-600 mt-10">
                 <div className='container m-auto py-2 text-sm text-black flex justify-between items-center'>
-                    <p className='mr-4'>Desarrollado con ❤️ por <b>Cecilia Perdomo</b></p>
-                    
-                    <div className='flex items-center gap-2'>
-                        <p className='text-black ml-2'>Powered by</p>
+                    <p className='ml-8'>
+                        Desarrollado con ❤️ por <b>
+                            <Link href="https://www.linkedin.com/in/cecilia-perdomo/">
+                                Cecilia Perdomo
+                            </Link>
+                        </b>
+                    </p>
+
+                    <div className='flex items-center gap-2 mr-4'>
+                        <p className='text-black'>Powered by</p>
                         <Image
                             src={"/Logo.png"}
                             alt={"Logo"}
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import ProductsList from "@/Components/productos/productList";
 export default function Home() {
   return (
     <main className="container m-auto">
-        <h1 className="text-2xl text-bold ml-6 mb-6 border-b pb-2">Nuestros productos ⭐ destacados:</h1>
-        
+        <h1 className="text-2xl text-bold ml-8 mb-6 border-b pb-2">Nuestros productos ⭐ destacados:</h1>
+
         <ProductsList categoria={"notebook"} />
         <br />
         <ProductsList categoria={"mouse"} />
